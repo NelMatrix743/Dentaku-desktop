f7df4841d12364c0a876b28accdac5aece477798
NelMatrix743 <nelmatrix155@gmail.com>
2025-02-28 20:49:52 +0000

    feat: Implement multiply() for multiplication operations

--- src/utils/math_functions.py
+++ src/utils/math_functions.py
@@ -1,7 +1,10 @@
 # MATH FUNCTIONS
 
 def add(first_num: int | float, second_num: int | float) -> int | float:
-    return firstnum + secondnum
+    return first_num + second_num
 
 def subtract(this_num: int | float, from_num: int | float) -> int | float:
     return from_num - this_num
+
+def multiply(first_num: int | float, second_num: int) -> int | float:
+    return first_num * second_num
