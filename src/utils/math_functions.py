3969c31d249972d78680ab7bdf48e9f862b204ce
NelMatrix743 <nelmatrix155@gmail.com>
2025-02-28 20:47:04 +0000

    feat: Implement the substract() function for subtraction operations

--- src/utils/math_functions.py
+++ src/utils/math_functions.py
@@ -3,3 +3,5 @@
 def add(first_num: int | float, second_num: int | float) -> int | float:
     return firstnum + secondnum
 
+def subtract(this_num: int | float, from_num: int | float) -> int | float:
+    return from_num - this_num
