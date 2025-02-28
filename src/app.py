34f4346df0d8cbc7e01826117bf89182f597d538
NelMatrix743 <nelmatrix155@gmail.com>
2025-02-28 17:37:16 +0000

    feat: Add template code for main ui

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QLineEdit
+from PyQt5.uic import loadUi
+from typing import Self
+
+
+class MainScreen(QMainWindow):
+
+    def __init__(self) -> Self:
+        super().__init__()
+        loadUi("ui_xml/main_screen.ui", self)
+
+
+    def launch(self) -> None:
+        self.show()
+
+
+if __name__ == "__main__":
+
+    app: QApplication = QApplication([])
+    main: MainScreen = MainScreen()
+    main.launch()
+    app.exec_()
+
+# eosc
