46e15d23c32c0a782ee9a452c434e199985cf86b
NelMatrix743 <nelmatrix155@gmail.com>
2025-02-28 21:03:40 +0000

    feat: Implement the divide() function for division operations

--- src/utils/math_functions.py
+++ src/utils/math_functions.py
@@ -3,8 +3,19 @@
 def add(first_num: int | float, second_num: int | float) -> int | float:
     return first_num + second_num
 
-def subtract(this_num: int | float, from_num: int | float) -> int | float:
+
+def substract(this_num: int | float, from_num: int | float) -> int | float:
     return from_num - this_num
+
 
 def multiply(first_num: int | float, second_num: int) -> int | float:
     return first_num * second_num
+
+
+def divide(this_num: int | float, by_num: int | float) -> int | float:
+    if isinstance(this_num, float) or isinstance(by_num, float):
+        # floating-point division
+        # round to 5 decimal digits
+        return round((this_num / by_num), 5)
+    # integer division
+    return this_num // by_num
