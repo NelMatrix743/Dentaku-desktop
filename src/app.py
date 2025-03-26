98d58d67fbdd080d5f366400087c075a92de10ac
NelMatrix743 <nelmatrix155@gmail.com>
2025-03-26 23:54:48 +0000

    feat(logic): Map digit and clear buttons to their functions

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from uipython.calculator_screen import CalculatorScreen
 from PyQt5.uic import loadUi
+from functools import partial
 from typing import Self
+from utils.special_functions import (
+    insert_value,
+    clear_last_input,
+    clear_screen
+)
 
 
 class MainScreen(QMainWindow):
@@ -40,11 +46,23 @@ class MainScreen(QMainWindow):
         self.six_button: QPushButton = self.findChild(QPushButton, "button_6")
         self.seven_button: QPushButton = self.findChild(QPushButton, "button_7")
         self.eight_button: QPushButton = self.findChild(QPushButton, "button_8")
-        self.nine_button: QPushButton = self.findChild(QPushButton, "button_p")
+        self.nine_button: QPushButton = self.findChild(QPushButton, "button_9")
         self.dot_button: QPushButton = self.findChild(QPushButton, "button_dot")
 
         # map buttons to functions
-        
+        self.zero_button.clicked.connect(partial(insert_value, "0", self.main_screen))
+        self.one_button.clicked.connect(partial(insert_value, "1", self.main_screen))
+        self.two_button.clicked.connect(partial(insert_value, "2", self.main_screen))
+        self.three_button.clicked.connect(partial(insert_value, "3", self.main_screen))
+        self.four_button.clicked.connect(partial(insert_value, "4", self.main_screen))
+        self.five_button.clicked.connect(partial(insert_value, "5", self.main_screen))
+        self.six_button.clicked.connect(partial(insert_value, "6", self.main_screen))
+        self.seven_button.clicked.connect(partial(insert_value, "7", self.main_screen))
+        self.eight_button.clicked.connect(partial(insert_value, "8", self.main_screen))
+        self.nine_button.clicked.connect(partial(insert_value, "9", self.main_screen))
+
+        self.clear_button.clicked.connect(partial(clear_last_input, self.main_screen))
+        self.all_clear_button.clicked.connect(partial(clear_screen, self.main_screen))
 
 
     def launch(self) -> None:
