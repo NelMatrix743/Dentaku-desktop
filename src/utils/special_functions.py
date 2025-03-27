39a171cf0b8a4ab5c04e70732689955b3eb4be0b
NelMatrix743 <nelmatrix155@gmail.com>
2025-03-27 00:00:47 +0000

    feat(logic): Implement screen clearing functions and data insertion function

--- src/utils/special_functions.py
+++ src/utils/special_functions.py
@@ -1,11 +1,25 @@
 from PyQt5.QtWidgets import QLabel
+from uipython.calculator_screen import CalculatorScreen
 
 
-def clear_screen(screen: QLabel) -> bool:
+def reset(screen: CalculatorScreen) -> None:
+    screen.setText('0')
+
+def clear_screen(screen: CalculatorScreen) -> None:
     screen.clear()
-    if len(screen.text()) == 0:
-        return True
-    return False
+    reset(screen)
 
+def clear_last_input(screen: CalculatorScreen) -> None:
+    content: str = screen.text()
+    content = content[0:len(content)-1]
+    if not len(content):
+        reset(screen)
+        return
+    screen.setText(content)
+
+def insert_value(value: str, screen: CalculatorScreen) -> None:
+    if screen.text().startswith("0"):
+        screen.clear()
+    screen.setText(screen.text() + value)
 
 # eosc
