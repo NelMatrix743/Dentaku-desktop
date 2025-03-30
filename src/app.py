1f1562053f20fb797b0df527e9875a9b5c801d87
NelMatrix743 <nelmatrix155@gmail.com>
2025-03-30 20:57:37 +0000

    refactor(logic): Improve code structure of app.py

--- src/app.py
+++ src/app.py
@@ -21,62 +21,69 @@ class MainScreen(QMainWindow):
         self.setup_ui_components()
 
 
+    def mapwidget(self, name: str, widget_type=QPushButton) -> any:
+        return self.findChild(widget_type, name)
+
+
+    def mapfunction(self, button: QPushButton, func: Callable, val=None) -> None:
+        if not val:
+            button.clicked.connect(partial(func, self.main_screen))
+            return
+        button.clicked.connect(partial(func, val, self.main_screen))
+
+
     def setup_ui_components(self) -> None:
         # calculator screens
-        self.main_screen: CalculatorScreen = self.findChild(CalculatorScreen, "expr_screen")
-        self.result_screen: CalculatorScreen = self.findChild(CalculatorScreen, "res_screen")
+        self.main_screen: CalculatorScreen = self.mapwidget("expr_screen", CalculatorScreen)
+        self.result_screen: CalculatorScreen = self.mapwidget("res_screen", CalculatorScreen)
 
         # function buttons
-        self.clear_button: QPushButton = self.findChild(QPushButton, "button_clr")
-        self.all_clear_button: QPushButton = self.findChild(QPushButton, "button_ac")
+        self.clear_button: QPushButton = self.mapwidget("button_clr")
+        self.all_clear_button: QPushButton = self.mapwidget("button_ac")
 
         # arithmetic operators
-        self.percentage_button: QPushButton = self.findChild(QPushButton, "button_perc")
-        self.division_button: QPushButton = self.findChild(QPushButton, "button_div")
-        self.multiplication_button: QPushButton = self.findChild(QPushButton, "button_mul")
-        self.subtraction_button: QPushButton = self.findChild(QPushButton, "button_min")
-        self.addition_button: QPushButton = self.findChild(QPushButton, "button_add")
-        self.equals_button: QPushButton = self.findChild(QPushButton, "button_eq")
-        self.plus_minus_button: QPushButton = self.findChild(QPushButton, "button_pm")
+        self.percentage_button: QPushButton = self.mapwidget("button_perc")
+        self.division_button: QPushButton = self.mapwidget("button_div")
+        self.multiplication_button: QPushButton = self.mapwidget("button_mul")
+        self.subtraction_button: QPushButton = self.mapwidget("button_min")
+        self.addition_button: QPushButton = self.mapwidget("button_add")
+        self.equals_button: QPushButton = self.mapwidget("button_eq")
+        self.plus_minus_button: QPushButton = self.mapwidget("button_pm")
 
         # digits and decimal point
-        self.zero_button: QPushButton = self.findChild(QPushButton, "button_0")
-        self.one_button: QPushButton = self.findChild(QPushButton, "button_1")
-        self.two_button: QPushButton = self.findChild(QPushButton, "button_2")
-        self.three_button: QPushButton = self.findChild(QPushButton, "button_3")
-        self.four_button: QPushButton = self.findChild(QPushButton, "button_4")
-        self.five_button: QPushButton = self.findChild(QPushButton, "button_5")
-        self.six_button: QPushButton = self.findChild(QPushButton, "button_6")
-        self.seven_button: QPushButton = self.findChild(QPushButton, "button_7")
-        self.eight_button: QPushButton = self.findChild(QPushButton, "button_8")
-        self.nine_button: QPushButton = self.findChild(QPushButton, "button_9")
-        self.dot_button: QPushButton = self.findChild(QPushButton, "button_dot")
+        self.zero_button: QPushButton = self.mapwidget("button_0")
+        self.one_button: QPushButton = self.mapwidget("button_1")
+        self.two_button: QPushButton = self.mapwidget("button_2")
+        self.three_button: QPushButton = self.mapwidget("button_3")
+        self.four_button: QPushButton = self.mapwidget("button_4")
+        self.five_button: QPushButton = self.mapwidget("button_5")
+        self.six_button: QPushButton = self.mapwidget("button_6")
+        self.seven_button: QPushButton = self.mapwidget("button_7")
+        self.eight_button: QPushButton = self.mapwidget("button_8")
+        self.nine_button: QPushButton = self.mapwidget("button_9")
+        self.dot_button: QPushButton = self.mapwidget("button_dot")
 
         # map buttons to functions
-        self.map_function(self.zero_button, insert_digit, "0")
-        self.map_function(self.one_button, insert_digit, "1")
-        self.map_function(self.two_button, insert_digit, "2")
-        self.map_function(self.three_button, insert_digit, "3")
-        self.map_function(self.four_button, insert_digit, "4")
-        self.map_function(self.five_button, insert_digit, "5")
-        self.map_function(self.six_button, insert_digit, "6")
-        self.map_function(self.seven_button, insert_digit, "7")
-        self.map_function(self.eight_button, insert_digit, "8")
-        self.map_function(self.nine_button, insert_digit, "9")
-        self.map_function(self.zero_button, insert_digit, "0")
+        self.mapfunction(self.zero_button, insert_digit, "0")
+        self.mapfunction(self.one_button, insert_digit, "1")
+        self.mapfunction(self.two_button, insert_digit, "2")
+        self.mapfunction(self.three_button, insert_digit, "3")
+        self.mapfunction(self.four_button, insert_digit, "4")
+        self.mapfunction(self.five_button, insert_digit, "5")
+        self.mapfunction(self.six_button, insert_digit, "6")
+        self.mapfunction(self.seven_button, insert_digit, "7")
+        self.mapfunction(self.eight_button, insert_digit, "8")
+        self.mapfunction(self.nine_button, insert_digit, "9")
+        self.mapfunction(self.zero_button, insert_digit, "0")
 
-        self.dot_button.clicked.connect(partial(insert_decimal_point, self.main_screen))
-        self.clear_button.clicked.connect(partial(clear_last_input, self.main_screen))
-        self.all_clear_button.clicked.connect(partial(clear_screen, self.main_screen))
+        self.mapfunction(self.dot_button, insert_decimal_point)
+        self.mapfunction(self.clear_button, clear_last_input)
+        self.mapfunction(self.all_clear_button, clear_screen)
 
-        self.addition_button.clicked.connect(partial(insert_operator, '+', self.main_screen))
-        self.subtraction_button.clicked.connect(partial(insert_operator, '-', self.main_screen))
-        self.multiplication_button.clicked.connect(partial(insert_operator, '×', self.main_screen))
-        self.division_button.clicked.connect(partial(insert_operator, '÷', self.main_screen))
-
-        
-    def map_function(self, button: QPushButton, func: Callable, val: str) -> None:
-        button.clicked.connect(partial(func, val, self.main_screen))
+        self.mapfunction(self.addition_button, insert_operator, "+")
+        self.mapfunction(self.multiplication_button, insert_operator, '×')
+        self.mapfunction(self.subtraction_button, insert_operator, "-")
+        self.mapfunction(self.division_button, insert_operator, '÷')
 
 
     def launch(self) -> None:
